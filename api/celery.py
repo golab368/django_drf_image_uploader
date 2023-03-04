5ba5a4fecb3f7bd01a9db022ebe1bc66facb9a1d
M <m@Ms-MacBook-Air.local>
2023-03-04 21:03:14 +0000

    cellery

--- api/celery.py
+++ api/celery.py
@@ -14,11 +14,12 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
 
-# Use Redis as the broker for Celery.
-app = Celery("api", broker=os.environ["REDIS_URL"])
-
-# Configure Celery using Django settings.
+app = Celery("api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Auto-discover tasks.
-app.autodiscover_tasks(["api.tasks"])
+# Update broker URL to use Redis
+app.conf.update(BROKER_URL=os.environ.get('REDIS_URL', 'redis://localhost:6379/0'))
+
+# Automatically discover tasks in the 'api.tasks' module
+app.autodiscover_tasks(['api.tasks'])
+
