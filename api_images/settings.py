471b870772967015a02945a080fe16db52a053c7
M <m@Ms-MacBook-Air.local>
2023-03-04 15:14:21 +0000

    heroku add settings

--- api_images/settings.py
+++ api_images/settings.py
@@ -154,8 +154,10 @@ MEDIA_URL = "/media/"
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 # #Redis Heroku
-CELERY_BROKER_URL = os.environ.get("REDIS_URL")
-CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
+# CELERY_BROKER_URL = os.environ.get("REDIS_URL")
+# CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
