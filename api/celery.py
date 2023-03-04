182295049d418c2925de92dab28d12cb0902dfb8
M <m@Ms-MacBook-Air.local>
2023-03-04 23:57:40 +0000

    celery..

--- api/celery.py
+++ api/celery.py
@@ -1,24 +1,12 @@
-# import os
-# from celery import Celery
-
-# # set the default Django settings module for the 'celery' program.
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
-
-
-# app = Celery("api")
-# app.config_from_object("django.conf:settings", namespace="CELERY")
-# app.autodiscover_tasks(["api.tasks"])
 import os
 from celery import Celery
 
-# Set the default Django settings module for the 'celery' program.
+# set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
 
-app = Celery("api", broker=os.environ['REDIS_URL'])
+
+app = Celery("api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks(["api.tasks"])
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-
-app.autodiscover_tasks(['api.tasks'])
 
