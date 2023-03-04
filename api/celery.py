ef3167f383e236104e53cbdd63432f2ea0182816
M <m@Ms-MacBook-Air.local>
2023-03-04 21:56:11 +0000

    cellery66

--- api/celery.py
+++ api/celery.py
@@ -14,7 +14,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
 
-app = Celery("api")
+app = Celery("api", broker=os.environ['REDIS_URL'])
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Update broker URL to use Redis
