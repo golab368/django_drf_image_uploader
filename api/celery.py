a9745147d1127c86c346d950712b15b3c234b9ab
M <m@Ms-MacBook-Air.local>
2023-03-04 19:00:19 +0000

    heroku

--- api/celery.py
+++ api/celery.py
@@ -7,5 +7,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
 
 app = Celery("api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 app.autodiscover_tasks(["api.tasks"])
--- api_images/settings.py
+++ api_images/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     "api",
     "django_heroku",
     "django_extensions",
-    "api.celery",
 ]
 
 MIDDLEWARE = [
@@ -163,8 +162,8 @@ CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
 
-# if os.environ.get("ENV") == "test":
-#     CELERY_TASK_ALWAYS_EAGER = True
+if os.environ.get("ENV") == "test":
+    CELERY_TASK_ALWAYS_EAGER = True
 
-CELERY_IMPORTS = ("api.tasks", )
+
 django_heroku.settings(locals())
