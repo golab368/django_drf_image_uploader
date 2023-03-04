87c610bb412202896ca13e859ba53cde02dcd9c9
M <m@Ms-MacBook-Air.local>
2023-03-04 18:40:17 +0000

    heroku add4

--- api/celery.py
+++ api/celery.py
@@ -4,6 +4,8 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
 
+
 app = Celery("api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
+app.conf.broker_url = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 app.autodiscover_tasks(["api.tasks"])
