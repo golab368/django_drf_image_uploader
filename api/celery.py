6627a4039cb4e0a3e0f15601ce533299f1b243ca
M <m@Ms-MacBook-Air.local>
2023-03-05 11:00:27 +0000

    c added a without setdefault

--- api/celery.py
+++ api/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_images.settings")
 
 app = Celery("api")
 app.config_from_object("django.conf:settings", namespace="CELERY")
